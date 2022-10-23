29f9af21e520be3b48b5311a6a6e0b23489bcfe4
Oleg Aushev <aushevom@gmail.com>
2022-10-23 12:57:02 +0300

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,33 @@
+/**
+ * @file main.cpp
+ * @author Oleg Aushev (aushevom@protonmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2022-10-23
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+
+
+
+
+
+
+
+int main()
+{
+
+}
+
+
+
+
+
+
+
+
+
+
+
