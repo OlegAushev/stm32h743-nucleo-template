4cbca28557dd2b08fc65677b7b013d48475cd6f2
Oleg Aushev <aushevom@gmail.com>
2022-11-02 16:55:21 +0300

    work on adc

--- include/mcu_stm32h7/adc/mcu_adc.h
+++ include/mcu_stm32h7/adc/mcu_adc.h
@@ -28,10 +28,31 @@ namespace mcu {
 namespace adc {
 
 
+struct PinConfig
+{
+	GPIO_TypeDef* port;
+	uint32_t pin;
+};
+
+
+struct Config
+{
+	ADC_InitTypeDef init;
+	ADC_ChannelConfTypeDef channel; 
+};
 
 
 
 
+
+namespace detail {
+	inline constexpr std::array<ADC_TypeDef*, 3> adcModules = {ADC1, ADC2, ADC3};
+	inline std::array<std::function<void(void)>, 3> adcClkEnableFuncs = {
+		[](){__HAL_RCC_ADC12_CLK_ENABLE();},
+		[](){__HAL_RCC_ADC12_CLK_ENABLE();},
+		[](){__HAL_RCC_ADC3_CLK_ENABLE();}	
+	};
+}
 
 
 
