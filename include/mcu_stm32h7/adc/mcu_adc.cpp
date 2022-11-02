42b6bc58fa1642f18a0d3aa476e5b227eb228b1b
Oleg Aushev <aushevom@gmail.com>
2022-11-02 13:41:00 +0300

    add mcu::adc template

---
+++ include/mcu_stm32h7/adc/mcu_adc.cpp
@@ -0,0 +1,18 @@
+/**
+ * @file mcu_adc.cpp
+ * @ingroup mcu mcu_adc
+ * @author Oleg Aushev (aushevom@protonmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2022-11-02
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+
+#include "mcu_adc.h"
+
+
+
+
---
+++ include/mcu_stm32h7/adc/mcu_adc.h
@@ -0,0 +1,46 @@
+/**
+ * @defgroup mcu_adc ADC
+ * @ingroup mcu
+ * 
+ * @file mcu_adc.h
+ * @ingroup mcu mcu_adc
+ * @author Oleg Aushev (aushevom@protonmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2022-11-02
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+
+#pragma once
+
+
+#include "../mcu_def.h"
+#include "../system/mcu_system.h"
+#include "../gpio/mcu_gpio.h"
+
+
+namespace mcu {
+
+
+namespace adc {
+
+
+
+
+
+
+
+
+
+
+
+
+} // namespace adc
+
+
+} // namespace mcu
+
+
